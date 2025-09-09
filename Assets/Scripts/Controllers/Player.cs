dd61f403589f7e0e7ac65ba84600fa9fdc01bcea
BrookieGG <brooke4444@me.com>
2025-09-09 14:37:13 -0400

    Spawn Bomb at Offset

---
+++ Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public List<Transform> asteroidTransforms;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            SpawnBombAtOffset(new Vector3(0, 1));
+        }
+    }
+
+    private void SpawnBombAtOffset(Vector3 inOffset) //Spawn Bomb at Offset
+    {
+        Vector3 spawnPosition = transform.position + inOffset;
+        Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
+    }
+}
